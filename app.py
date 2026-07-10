1f82db0a72fee56db982a1949d4ca20823757408
shreyahtech <shreyaa.h553@gmail.com>
2026-07-10 13:45:25 +0530

    fix app.py again

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ import pickle
 import string
 import nltk
 
-nltk.download('punkt')
-nltk.download('punkt_tab')
-nltk.download('stopwords')
+nltk.download('punkt', quiet=True)
+nltk.download('punkt_tab', quiet=True)
+nltk.download('stopwords', quiet=True)
 
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
