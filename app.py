f1719bee85dc82b4da32072067853f25d42e7ce8
shreyahtech <shreyaa.h553@gmail.com>
2026-07-10 13:14:35 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,60 @@
+import streamlit as st
+import pickle
+import string
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+
+# Load model and vectorizer
+tfidf = pickle.load(open('vectorizer.pkl', 'rb'))
+model = pickle.load(open('model.pkl', 'rb'))
+
+ps = PorterStemmer()
+
+# Text preprocessing function
+def transform_text(text):
+    text = text.lower()
+    text = nltk.word_tokenize(text)
+
+    y = []
+    for i in text:
+        if i.isalnum():
+            y.append(i)
+
+    text = y[:]
+    y.clear()
+
+    for i in text:
+        if i not in stopwords.words('english') and i not in string.punctuation:
+            y.append(i)
+
+    text = y[:]
+    y.clear()
+
+    for i in text:
+        y.append(ps.stem(i))
+
+    return " ".join(y)
+
+# Streamlit UI
+st.set_page_config(page_title="SMS Spam Classifier", page_icon="📩")
+
+st.title("📩 SMS Spam Classifier")
+
+input_sms = st.text_area("Enter your message")
+
+if st.button("Predict"):
+
+    # Preprocess
+    transformed_sms = transform_text(input_sms)
+
+    # Vectorize
+    vector_input = tfidf.transform([transformed_sms])
+
+    # Predict
+    result = model.predict(vector_input)[0]
+
+    if result == 1:
+        st.error("Spam Message")
+    else:
+        st.success("Not Spam")
